ba26601864f9d7b7ca7c913c7da5ef15054d5889
iambenbright <yankeybenneth@gmail.com>
2020-07-12 14:14:01 +0000

    feat: 'adds index.js and link to html'

---
+++ easy-bank-landing-page/js/index.js
@@ -0,0 +1,29 @@
+function init() {
+  const menuToggle = document.querySelector('[data-menu="close"]');
+  const allHidden = document.querySelectorAll('.is-not-visible');
+
+  let menuIsOpen = false;
+
+  function handleMenuToggle(event) {
+    let menu = event.target;
+
+    const setAttributes = (src, alt) => {
+      menu.setAttribute('src', src);
+      menu.setAttribute('alt', alt);
+    };
+
+    if (!menuIsOpen) {
+      setAttributes('images/icon-close.svg', 'icon close');
+      [...allHidden].forEach(elem => elem.classList.remove('is-not-visible'));
+      menuIsOpen = true;
+    } else {
+      setAttributes('images/icon-hamburger.svg', 'icon hamburger');
+      [...allHidden].forEach(elem => elem.classList.add('is-not-visible'));
+      menuIsOpen = false;
+    }
+  }
+
+  menuToggle.addEventListener('click', handleMenuToggle);
+}
+
+window.addEventListener('DOMContentLoaded', init);
